3f25428fc2ae3a6f219631b3f482b83302f9c11b
GuilhermeMuniz10 <gm8074114@gmail.com>
2024-09-21 23:33:13 -0300

    dei uma mexida no cabeçalho e estou deixando o carrossel em andamento

---
+++ src/paginainicial/paginicio.jsx
@@ -0,0 +1,90 @@
+import './paginicio.scss';
+
+function Paginicio() {
+  return (
+    <div className="paginicio">
+
+
+     <header>
+          <div className='cabecalho'>
+            <a href="">Home</a>
+            <a href="">Nossa História</a>
+            <a href="">Serviços</a>
+            <a href="">Galeria</a>
+            <a href="">Produtos</a>
+
+            <a className='log' href="">Login</a>
+          </div>
+     </header>
+
+     <div className='slider'>
+ 
+         <div className='slides'>
+
+
+               {/* Botões de radios */}
+               <input type="radio" name="radio-btn" id="radio1" />
+               <input type="radio" name="radio-btn" id="radio2" />
+               <input type="radio" name="radio-btn" id="radio3" />
+               <input type="radio" name="radio-btn" id="radio4" />
+               {/* Fim Botão de radio */}
+               
+
+               {/* imagens de slides  */}
+               <div className="primeiro slide">
+                  <img src="teste.png" alt="Imagem 1"/>
+               </div>
+
+               <div className="slide">
+                  <img src="teste.png" alt="Imagem 2"/>
+               </div>
+
+               <div className="slide">
+                  <img src="teste.png" alt="Imagem 3"/>
+               </div>
+
+               <div className="slide">
+                  <img src="teste.png" alt="Imagem 4"/>
+               </div>
+
+                {/* Fim imagens de slides  */}
+
+                {/* Navegação automatica */}
+                <div className='navegacao auto'>
+                     <div className="auto-btn1"></div>
+                     <div className="auto-btn2"></div>
+                     <div className="auto-bt3"></div>
+                     <div className="auto-bt4"></div>
+                     
+                </div>
+
+         </div>
+
+
+         <div className="manual-navegacao">
+          <label For="radio1" className='manual-btn'></label>
+          <label For="radio2" className='manual-btn'></label>
+          <label For="radio3" className='manual-btn'></label>
+          <label For="radio4" className='manual-btn'></label>
+          
+         </div>
+   
+
+     </div>
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default Paginicio;
+
