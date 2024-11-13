4d7950e3cbc47d7cec3bccb0ee9610125199d4f2
gabialbertassi <gabialbertassi01@gmail.com>
2024-11-13 18:15:53 -0300

    sbd footer

--- src/paginainicial/paginicio.jsx
+++ src/paginainicial/paginicio.jsx
@@ -254,12 +254,48 @@ function Paginicio() {
 
       
          <div className='mapBox'>
-         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.189356950853!2d-46.70332172578109!3d-23.704931066880288!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce4fb7e753c599%3A0x6f9ca28fe0093bd2!2sAut%C3%B3dromo%20de%20Interlagos%20-%20Port%C3%A3o%208%20%2F%20Gate%208!5e0!3m2!1spt-BR!2sbr!4v1728855956689!5m2!1spt-BR!2sbr"    allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3653.048585268878!2d-46.69951782396472!3d-23.709958967069635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce4fbbad0ba35b%3A0xd513dad5edc66c5f!2sR.%20Casqueiro%2C%20208%20-%20Cidade%20Dutra%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2004810-090!5e0!3m2!1spt-BR!2sbr!4v1731530406401!5m2!1spt-BR!2sbr"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
          </div>
 
 
 
          <footer>
+
+            <div className='conteiners'> 
+                        <div className='cont'>
+                           <img src="/assets/images/iconeLO.png" alt="" />
+                           <div>
+                              <p>Nosso Endereço:</p>
+                           <p>R. Casqueiro, 208 | Cidade Dutra</p>
+                           <p>CEP: 04810-090</p>
+                           </div>
+                           
+                        </div>
+
+                        <div className='plt'>|</div>
+
+                        <div className='cont'>
+                           <img src="/assets/images/iconeTEL.png" alt="" />
+                           <div>
+                              <p>Tel.: +55 (11) 9999-9999</p>
+                              <p>Cel.: +55 (11) 9999-9999</p>
+                           </div>
+                        </div>
+
+                        <div className='plt'>|</div>
+
+
+                        <div className='cont'>
+                        <img src="/assets/images/iconeEmail.png" alt="" />
+                           <div>
+                              <p>Segue o E-mail:</p>
+                              <p>adrianagomes@gmail.com</p>
+                           </div>
+                        </div>
+
+            </div>
+
+            
             <h1>Site desenvolvido pela agência VeloxByte</h1>
          </footer>
 
